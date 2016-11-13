a34e1adafb5b2338980bcc93e3ebada439ce0c3d
Charmos <charmavena@gmail.com>
2016-11-13 10:04:34 +0800

    Added home section, animated scrolling, fixed twwet styling.

--- js/index.js
+++ js/index.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
-  var chart = Highcharts.chart('container', {
-    chart: {
-      type: 'column'
-    },
-    title: {
-      text: 'Population of Countries'
-    },
-    subtitle: {
-      text: 'Source: WorldClimate.com'
-    },
-    xAxis: {
-      categories: [
-        '2016 Population'
-      ],
-      crosshair: true
-    },
-    yAxis: {
-      min: 0,
-      title: {
-        text: 'Population Count'
-      }
-    },
-    tooltip: {
-      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-        '<td style="padding:0"><b>{point.y:.1f} people</b></td></tr>',
-      footerFormat: '</table>',
-      shared: true,
-      useHTML: true
-    },
-    plotOptions: {
-      column: {
-        pointPadding: 0.2,
-        borderWidth: 0
-      }
-    },
-    series: [{"name": "Glenn","data":[500,800]}, {"name": "Charm","data":[700,500]}, {"name": "Glenn","data":[500,800]}, {"name": "Charm","data":[700,500]}, {"name": "Glenn","data":[500,800]}, {"name": "Charm","data":[700,500]}, {"name": "Glenn","data":[500,800]}, {"name": "Charm","data":[700,500]}, {"name": "Glenn","data":[500,800]}, {"name": "Charm","data":[700,500]}]
-  });
+  // var chart = Highcharts.chart('container', {
+  //   chart: {
+  //     type: 'column'
+  //   },
+  //   title: {
+  //     text: 'Population of Countries'
+  //   },
+  //   subtitle: {
+  //     text: 'Source: WorldClimate.com'
+  //   },
+  //   xAxis: {
+  //     categories: [
+  //       '2016 Population'
+  //     ],
+  //     crosshair: true
+  //   },
+  //   yAxis: {
+  //     min: 0,
+  //     title: {
+  //       text: 'Population Count'
+  //     }
+  //   },
+  //   tooltip: {
+  //     headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+  //     pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+  //       '<td style="padding:0"><b>{point.y:.1f} people</b></td></tr>',
+  //     footerFormat: '</table>',
+  //     shared: true,
+  //     useHTML: true
+  //   },
+  //   plotOptions: {
+  //     column: {
+  //       pointPadding: 0.2,
+  //       borderWidth: 0
+  //     }
+  //   },
+  //   series: [{"name": "Glenn","data":[500,800]}, {"name": "Charm","data":[700,500]}, {"name": "Glenn","data":[500,800]}, {"name": "Charm","data":[700,500]}, {"name": "Glenn","data":[500,800]}, {"name": "Charm","data":[700,500]}, {"name": "Glenn","data":[500,800]}, {"name": "Charm","data":[700,500]}, {"name": "Glenn","data":[500,800]}, {"name": "Charm","data":[700,500]}]
+  // });
 
   var dateTweeted;
 
@@ -48,4 +48,16 @@ $(document).ready(function() {
         $(this).attr('src', 'img/twitter-logo.png');
     });
   }
+
+  $('#homePage').click(function() {
+    var target = $('#tweetPage');
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+
+  });
 });
