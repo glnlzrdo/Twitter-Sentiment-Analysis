52620d3ef87a1aebe16e753e0cf00efe9b467f23
Charmos <charmavena@gmail.com>
2016-11-13 16:25:13 +0800

    Fixed reload. Added next and previous buttons.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 (function(document, window, $, corpus, data) {
   'use strict';
 
+  var PAGE_SIZE = 10;
+  var CURRENT_PAGE = 0;
+  var MAX_PAGES = Math.ceil(data.length/PAGE_SIZE);
+
 
   function computeSentiment(tweetStr, corpusData) {
     // remove commonly encountered words
@@ -26,6 +30,7 @@
       return arr.length > 0;
     })
     .map(function(word) {
+      console.log(word);
       return corpusData[word];
     })
     .reduce(function(a, b) {
@@ -39,11 +44,11 @@
   function mapSentimentToIcon(sentiment) {
 
     if(sentiment === 0) {
-      return ':|';
+      return 'meh';
     } else if (sentiment < 0) {
-      return ':(';
+      return 'sad';
     } else {
-      return ':)';
+      return 'happy';
     }
   };
 
@@ -53,32 +58,41 @@
     var dateTweeted;
 
 
-    for (var i = 0; i < 10; i++) {
-      dateTweeted = new Date(data[i].created_at);
-      var tweet = data[i];
-      var buttonStr = '<button class="follow-button">Follow</button>';
-      var imgStr = '<img class="profile-pic" height="48" width="48" src="' + tweet.user.profile_image_url + '" />';
-      var userNameStr = '<label class="user">' + tweet.user.name + '</label>';
-      var aliasStr = '<label class="alias">@' + tweet.user.screen_name + '</label>';
+    function renderTweets(tweetsArray, pageNumber, pageSize) {
+      var tweetsContainer = $('#tweets-container');
 
-      $("#tweets-container").append(
-        '<div class="tweet-container">'+
-          buttonStr+imgStr+userNameStr+'<br />'+
-          aliasStr+'<br />'+
-          '<label class="user-tweet">' + tweet.text + '</label>'+
-          '<label class="tweet-time">' + tweet.created_at + '</label>'+
-          '<div>' + mapSentimentToIcon(computeSentiment(tweet.text, corpus)) + '</div>' +
-        '</div>'
-      );
+      tweetsContainer[0].innerHTML = '';
 
-      $(".profile-pic").on("error", function(){
-          $(this).attr('src', 'img/twitter-logo.png');
-      });
+      for (var i = pageSize*pageNumber; i < pageSize*(pageNumber +1); i++) {
+        var tweet = tweetsArray[i];
+        // render only when tweet is defined
+        if(tweet) {
+          var dateTweeted = new Date(tweet.created_at);
+          var buttonStr = '<button class="follow-button">Follow</button>';
+          var imgStr = '<img class="profile-pic" height="48" width="48" src="' + tweet.user.profile_image_url + '" />';
+          var userNameStr = '<label class="user">' + tweet.user.name + '</label>';
+          var aliasStr = '<label class="alias">@' + tweet.user.screen_name + '</label>';
+
+          tweetsContainer.append(
+            '<div class="tweet-container">'+
+              buttonStr+imgStr+userNameStr+'<br />'+
+              aliasStr+'<br />'+
+              '<label class="user-tweet">' + tweet.text + '</label>'+
+              '<label class="tweet-time">' + tweet.created_at + '</label>'+
+              '<div class="sentiment-icon"><img src="./img/' + mapSentimentToIcon(computeSentiment(tweet.text, corpus)) + '.png"/></div>' +
+            '</div>'
+          );
+
+          $(".profile-pic").on("error", function(){
+              $(this).attr('src', 'img/twitter-logo.png');
+          });
+        }
+      }
     }
 
-    // animated scrolling
-    $('#homePage').click(function() {
-      var target = $('#tweetPage');
+
+    function scroll(target) {
+
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
@@ -86,8 +100,41 @@
         }, 1000);
         return false;
       }
+    }
+    // animated scrolling
+    $('#homePage').click(function() {
+      scroll($('#tweetPage'));
 
     });
+
+    $('#nextButton').click(function() {
+      if(CURRENT_PAGE + 1 < MAX_PAGES) {
+        CURRENT_PAGE++;
+        renderTweets(data, CURRENT_PAGE, PAGE_SIZE);
+        $('#previousButton').attr('disabled', false);
+        scroll($('#tweetPage'));
+      } else {
+        console.log('AA');
+        $(this).attr('disabled', true);
+      }
+    });
+
+    $('#previousButton').click(function() {
+      if(CURRENT_PAGE - 1 >= 0) {
+        CURRENT_PAGE--;
+        renderTweets(data, CURRENT_PAGE, PAGE_SIZE);
+
+        $('#nextButton').attr('disabled', false);
+        scroll($('#tweetPage'));
+      } else {
+        console.log('AA');
+        $(this).attr('disabled', true);
+      }
+    })
+
+      scroll($('#homePage'));
+      renderTweets(data, CURRENT_PAGE, PAGE_SIZE); //render first page
+      $('#previousButton').attr('disabled', true);
   });
 
 }(document, window, $, window.corpus, window.data));
